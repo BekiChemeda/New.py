2d4ac543a8fd5e05655c13485948912e33399e3f
BekiChemeda <122085319+BekiChemeda@users.noreply.github.com>
2023-10-02 22:28:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def start_message(msg):
     name = msg.from_user.first_name
     id = msg.from_user.id
     username = msg.from_user.username
-    bot.reply_to(msg, "   Your Information \n  Id : "+str(id)+" \n Name : "+ name +"\n username = @" + username)
+    bot.reply_to(msg, "   Your Information \n  Id : "+str(id)+" \n Name : "+ name +"\n username = @" + username + "\n \n @BC_bot_projects")
     print("1")
 print("running...")
 bot.polling()
